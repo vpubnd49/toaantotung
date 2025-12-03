deb8825a334e606ac38b57481b97ba40375870c7
vpubnd49 <haichau2404@gmail.com>
2025-12-03 21:46:19 +0700

    Add files via upload

---
+++ constants.ts
@@ -0,0 +1,9 @@
+
+export const COURTS = [
+  'TAND TP.HCM',
+  'TAND Cấp Cao',
+  'TAND Quận 1',
+  'TAND Quận 7',
+  'TAND TP.Thủ Đức',
+  'TAND tỉnh Lâm Đồng',
+];
---
+++ firebaseConfig.ts
@@ -0,0 +1,10 @@
+
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyB6m-uO8Kfjn0PtxuHqHP2qfY6InyHEInI",
+  authDomain: "toaantotung.firebaseapp.com",
+  projectId: "toaantotung",
+  storageBucket: "toaantotung.firebasestorage.app",
+  messagingSenderId: "875081176434",
+  appId: "1:875081176434:web:b3b1c94d42d6dcc877f9eb",
+  measurementId: "G-9NB1HV0N4F"
+};
